63f9fca73d7328c83508dbdb6ad99b72247a6999
dellsystem <ilostwaldo@gmail.com>
2018-03-30 16:39:02 +0100

    Add metadata to articles in RSS feed
    Date, author name, categories.

--- django/journal/feeds.py
+++ django/journal/feeds.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.syndication.views import Feed
 
 from journal.models import Article
@@ -16,3 +18,12 @@ class ArticleFeed(Feed):
 
     def item_description(self, item):
         return item.subtitle
+
+    def item_pubdate(self, item):
+        return datetime.datetime.combine(item.date, datetime.time())
+
+    def item_author_name(self, item):
+        return ', '.join(a.name for a in item.authors.all()) or 'Anonymous'
+
+    def item_categories(self, item):
+        return [tag.name for tag in item.tags.all()]
