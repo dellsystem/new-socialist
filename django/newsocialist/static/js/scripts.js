55bb8f0d2ed1b16ad566689420aa2a2bb0239704
dellsystem <ilostwaldo@gmail.com>
2018-04-03 15:13:27 +0100

    Fix header background expansion on mobile
    Builds on d979e09a12fbb677a25efab1694e2e92a5c23890 (i.e., fixes a bug
    accidentally created by)
    
    Otherwise, the background image doesn't expand as the menu does.
    
    This commit also simplifies the JS.

--- django/newsocialist/static/js/scripts.js
+++ django/newsocialist/static/js/scripts.js
@@ -1,7 +1,5 @@
 function toggleHamburger() {
-    var rightMenus = document.querySelectorAll('.right.menu');
-    rightMenus[0].classList.toggle('visible');
-    rightMenus[1].classList.toggle('visible');
+    $('#header .right.menu').toggleClass('visible');
     return false;
 };
 
