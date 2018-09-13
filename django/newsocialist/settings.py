b91614026533422c4c3bcea5126b9187cd5bc559
dellsystem <ilostwaldo@gmail.com>
2018-09-13 14:26:12 +0100

    Add context processor for SITE_URL variable

--- django/newsocialist/settings.py
+++ django/newsocialist/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'newsocialist.context_processors.site',
             ],
         },
     },
